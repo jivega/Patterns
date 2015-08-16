3b8cfd389dd6b3b3bf9be81615e64bfb8f2038b2
jorge <jivega@yahoo.com>
2015-08-16 22:25:13 +0100

    The test fails from time to time

--- src/test/java/com/jivega/patterns/singleton/SingletonTest.java
+++ src/test/java/com/jivega/patterns/singleton/SingletonTest.java
@@ -5,33 +5,39 @@ import org.junit.Test;
 
 public class SingletonTest {
 	@Test
-    public void testOneInstanceSingleton()
-    {
+	public void testOneInstanceSingleton() {
 		Runnable run = new Runnable() {
 			public void run() {
-		        try {
-		            for (int i = 0; i < 200; i++) {
-		                Thread.sleep(1);
-		    			SingletonOne singletonOne = SingletonOne.getInstance();
-		                //System.out.print(i + "\n" + "...");
-		            }
-		        } catch (Exception e) {
-		            System.out.println(" interrupted");
-		        }
-		    }
+				try {
+					Thread.sleep(10);
+					for (int i = 0; i < 200; i++) {
+						//Thread.sleep(0);
+						SingletonOne singletonOne = SingletonOne.getInstance();
+						// System.out.print(i + "\n" + "...");
+					}
+				} catch (Exception e) {
+					System.out.println(" interrupted");
+				}
+			}
 		};
-		 new Thread(run).start();
-		 new Thread(run).start();
-		 new Thread(run).start();
-		 new Thread(run).start();
-		 try{
-			 Thread.sleep(1000);	 
-		 } catch (Exception e) {
-			 System.out.println("Error in sleep");
-		 }
-		 
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		new Thread(run).start();
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			System.out.println("Error in sleep");
+		}
 		Integer numberInstances = SingletonOne.getNumberOfInstances();
-		
-        Assert.assertEquals( Integer.valueOf(1), numberInstances);
-    }
+		Assert.assertEquals(Integer.valueOf(1), numberInstances);
+	}
 }
